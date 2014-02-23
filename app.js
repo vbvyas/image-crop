016b3b943e9d621755e0b56c37c86e43ddda266d
vinay <vbvyas@gmail.com>
2014-02-23 04:48:51 -0800

    Added imagemagick backend cropping and resizing

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ app.get('/uploads/full/:file', function (req, res){
 	res.writeHead(200, {'Content-Type': 'image/jpg' });
 	res.end(img, 'binary');
 });
+app.get('/uploads/crop/:file', function (req, res){
+	file = req.params.file;
+	var img = fs.readFileSync(__dirname + "/routes/uploads/crop/" + file);
+	res.writeHead(200, {'Content-Type': 'image/jpg' });
+	res.end(img, 'binary');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
+var util = require('util');
 var fs = require('fs');
+var im = require('imagemagick');
+require('sugar');
 
 exports.index = function(req, res){
   res.render('index', { title: 'image-crop' });
@@ -11,22 +14,56 @@ exports.crop_image = function(req, res){
 
   fs.readFile(req.files.image.path, function(err, data){
     var imageName = req.files.image.name;
+    var imagePath = req.files.image.path;
+
+    var x1 = req.body.x1;
+    var y1 = req.body.y1;
+    var width = req.body.width;
+    var height = req.body.height;
+    var cropDimensions = util.format("%sx%s+%s+%s", width, height, x1, y1);
+    console.log("crop dimensions:", cropDimensions);
 
     if(!imageName) {
       console.log("ERROR");
       res.redirect('/');
       res.end();
     } else {
+
+      imageName = Number.random(100, 10000) + ".png";
       var newPath = __dirname + "/uploads/full/" + imageName;
+      var cropPath = __dirname + "/uploads/crop/" + imageName;
+      var thumbPath = __dirname + "/uploads/thumb/" + imageName;
+
       console.log("Path:", newPath);
-      fs.writeFile(newPath, data, function(err) {
+      fs.writeFileSync(newPath, data);
+
+      // resize to thumb size: 200px * 200px
+      im.resize({
+        srcPath: imagePath,
+        dstPath: thumbPath,
+        width: 200,
+      }, function (err, stdout, stderr) {
         if (err) {
-          console.log("ERROR:", err);
-          res.redirect('/');
-        } else {
-          res.redirect('/uploads/full/' + imageName);
+          console.log("image resizing error:" , err);
         }
       });
+
+      var cropArgs = [
+        imagePath,
+        "-crop",
+        cropDimensions,
+        cropPath,
+      ];
+
+      im.convert(cropArgs, function(err) {
+        if (err) {
+          console.log("Crop ERROR:", err);
+        } else {
+          console.log("Image crop complete");
+          res.redirect('/uploads/crop/' + imageName);
+        }
+      });
+
     }
   });
 };
