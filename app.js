c5296d0278b12066ae4957971e11d574b1c2fc2c
vinay <vbvyas@gmail.com>
2014-02-23 03:21:52 -0800

    Able to read and write files to the server.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
+  , fs = require('fs')
   , path = require('path');
 
 var app = express();
@@ -23,6 +24,14 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.post('/', routes.crop_image);
+
+// to be able to see the images after being saved
+app.get('/uploads/full/:file', function (req, res){
+	file = req.params.file;
+	var img = fs.readFileSync(__dirname + "/routes/uploads/full/" + file);
+	res.writeHead(200, {'Content-Type': 'image/jpg' });
+	res.end(img, 'binary');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,24 @@ exports.crop_image = function(req, res){
 
   console.log("file", req.files.image.name);
 
+  fs.readFile(req.files.image.path, function(err, data){
+    var imageName = req.files.image.name;
 
-  res.redirect('/');
+    if(!imageName) {
+      console.log("ERROR");
+      res.redirect('/');
+      res.end();
+    } else {
+      var newPath = __dirname + "/uploads/full/" + imageName;
+      console.log("Path:", newPath);
+      fs.writeFile(newPath, data, function(err) {
+        if (err) {
+          console.log("ERROR:", err);
+          res.redirect('/');
+        } else {
+          res.redirect('/uploads/full/' + imageName);
+        }
+      });
+    }
+  });
 };
